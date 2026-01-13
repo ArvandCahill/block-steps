117e3c2d593c2f6c4d70e1c4b23ef728d2559a11
AlcaFu <148020390+AlcaFu@users.noreply.github.com>
2026-01-13 20:32:24 +0700

    Reapply "Merge branch 'main' of https://github.com/ArvandCahill/Game-UKK"
    This reverts commit 5c26c703ac6374a16307eb8ea8be04dfd94c9bf3.

--- Assets/Scripts/Gameplay/World/Interactable/SolidBlock.cs
+++ Assets/Scripts/Gameplay/World/Interactable/SolidBlock.cs
@@ -1,14 +1,9 @@
 using UnityEngine;
 
-public class SolidBlock : Block, IDraggable
+public class SolidBlock : Block
 {
     public override void OnInteract(Vector3 position)
     {
         // Solid blocks cannot be interacted with
-    }
-
-    public void OnDrag(Vector3 position)
-    {
-        gameObject.transform.position = position;
     }
 }

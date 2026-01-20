8dec9ac4f81e8ffda519ac8e1dd8a20cad87da6c
Arvand Cahil <arvand.git@gmail.com>
2026-01-20 09:20:20 +0700

    Add Button Script

---
+++ Assets/Scripts/UI/Button.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    [SerializeField] private ButtonType buttonType;
+
+    private bool isPopupActive = false;
+
+    void OnClick(string name)
+    {
+        switch (buttonType)
+        {
+            case ButtonType.LoadScene:
+                GameManager.instance.SceneLoader.LoadScene(name);
+                break;
+            case ButtonType.UIAction:
+                ShowPopup(name);
+                break;
+        }
+    }
+
+    void ShowPopup(string name)
+    {
+        if (!isPopupActive)
+        {
+            isPopupActive = true;
+            GameManager.instance.uiManager.ShowPopup(name, true);
+        }
+
+        else GameManager.instance.uiManager.HidePopup(name);
+    }
+}

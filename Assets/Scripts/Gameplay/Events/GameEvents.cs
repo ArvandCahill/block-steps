169a69a7b11c178f6c64a99811d25fe966d0ddb8
Arvand Cahil <arvand.git@gmail.com>
2026-01-20 13:47:53 +0700

    Add Game Event

---
+++ Assets/Scripts/Gameplay/Events/GameEvents.cs
@@ -0,0 +1,24 @@
+using System;
+using UnityEngine;
+
+public static class GameEvents
+{
+    public static event Action OnGamePaused;
+    public static event Action<Vector3> OnPlayerMoving;
+    public static event Action<Vector3> OnPlayerStopped;
+
+    public static void TriggerGamePaused()
+    {
+        OnGamePaused?.Invoke();
+    }
+
+    public static void TriggerPlayerMoving(Vector3 targetPos)
+    {
+        OnPlayerMoving?.Invoke(targetPos);
+    }
+
+    public static void TriggerPlayerStopped(Vector3 stopPos)
+    {
+        OnPlayerStopped?.Invoke(stopPos);
+    }
+}

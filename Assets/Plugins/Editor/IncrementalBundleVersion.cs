e9be3e5da04058359cf380cdb96ed2d27478bc3f
Arvand Cahil <arvand.git@gmail.com>
2026-04-05 02:51:00 +0700

    Added Incremental Bundle Version

---
+++ Assets/Plugins/Editor/IncrementalBundleVersion.cs
@@ -0,0 +1,24 @@
+﻿using UnityEditor;
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+
+public class IncrementalBundleVersion : IPreprocessBuildWithReport
+{
+    public int callbackOrder => 0;
+
+    public void OnPreprocessBuild(BuildReport report)
+    {
+        if (report.summary.platform == BuildTarget.Android && EditorUserBuildSettings.buildAppBundle)
+        {
+            IncrementVersionCode();
+        }
+    }
+
+    private static void IncrementVersionCode()
+    {
+        int currentVersion = PlayerSettings.Android.bundleVersionCode;
+        PlayerSettings.Android.bundleVersionCode = currentVersion + 1;
+        Debug.Log($"Auto-Increment Version Code for AAB: {currentVersion} → {PlayerSettings.Android.bundleVersionCode}");
+    }
+}
